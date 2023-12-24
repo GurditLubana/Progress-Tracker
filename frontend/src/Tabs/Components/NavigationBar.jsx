30ef237b5b7bb1804bcdada5efb21a3f556000f9
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2023-12-23 21:55:11 -0600

    NavBar created

---
+++ frontend/src/Tabs/Components/NavigationBar.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const NavigationBar = () => {
+  return (
+    <>
+      <Navbar expand="lg" className="navBar">
+        <Container>
+          <Navbar.Brand className="white-text" href="#home">
+            Progress Tracker
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              className="navBar"
+              variant="underline"
+              defaultActiveKey="/home"
+            >          
+              <Nav.Link className="white-text" href="#home">
+                Home
+              </Nav.Link>
+              <Nav.Link className="white-text" href="#link">
+                Link
+              </Nav.Link>
+
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavigationBar;
---
+++ frontend/src/Tabs/ProgressTab.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import NavigationBar from "./Components/NavigationBar"
+const ProgressTab = () => {
+  return (
+    <div>
+      <NavigationBar></NavigationBar>
+    </div>
+  )
+}
+
+export default ProgressTab
